83759a577112013a99e36d0a42d4f50c0b30efdd
KOTLA LATHISH <latishkumarkotla781@gmail.com>
2025-04-05 16:12:04 +0530

    Update db_connect.php

--- WOMENS_COMPLAINTS_FORM_PHP_CODE/db_connect.php
+++ WOMENS_COMPLAINTS_FORM_PHP_CODE/db_connect.php
@@ -1,14 +1,1 @@
-<?php
-$host = "127.0.0.1";  // Server (localhost for local development)
-$user = "root";  // Default MySQL username
-$password = "";  // Leave empty for XAMPP, or add your MySQL password if set
-$dbname = "womens_complaints";  // Name of your database
 
-// Create a connection
-$conn = new mysqli($host, $user, $password, $dbname, 3307);
-
-// Check if the connection was successful
-if ($conn->connect_error) {
-    die("Database Connection Failed: " . $conn->connect_error);
-}
-?>
